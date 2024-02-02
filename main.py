27a28bb82a7927563201c551c35a0aa92ef26dfb
raulcad <raulcd@gmail.com>
2024-02-02 14:41:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Your name? ")
+city = input("Your city? ")
+favouriteFood = input("Your favourite food? ")
+favouriteMusic = input("Your favourite music? ")
+print("Your name is " + name + " and you live in " + city)
+print("You're probably hungry for " + favouriteFood + " while listening to " + favouriteMusic)
